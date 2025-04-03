3aa21415c48a400b5a2dcfbaf5d2f59fe912800e
CatButtes <ed@buttes.dev>
2025-04-03 17:50:44 +0100

    feat: Add otlp exporter

--- setup.go
+++ setup.go
@@ -12,6 +12,9 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	lg "go.opentelemetry.io/otel/log"
 	"go.opentelemetry.io/otel/log/global"
 	mt "go.opentelemetry.io/otel/metric"
@@ -172,6 +175,82 @@ func newPropagator() propagation.TextMapPropagator {
 }
 
 func newTracerProvider(res resource.Resource) (*trace.TracerProvider, error) {
+	_, otelAvailable := os.LookupEnv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if otelAvailable {
+		return newOtlpTracerProvider(res)
+	} else {
+		return newStdOutTracerProvider(res)
+	}
+}
+
+func newMeterProvider(res resource.Resource) (*metric.MeterProvider, error) {
+	_, otelAvailable := os.LookupEnv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if otelAvailable {
+		return newOtlpMeterProvider(res)
+	} else {
+		return newStdOutMeterProvider(res)
+	}
+}
+
+func newLoggerProvider(res resource.Resource) (*log.LoggerProvider, error) {
+	_, otelAvailable := os.LookupEnv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if otelAvailable {
+		return newOtlpLoggerProvider(res)
+	} else {
+		return newStdOutLoggerProvider(res)
+	}
+}
+
+func newStdOutTracerProvider(res resource.Resource) (*trace.TracerProvider, error) {
+	traceExporter, err := stdouttrace.New(
+		stdouttrace.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithResource(&res),
+		trace.WithBatcher(traceExporter,
+			// Default is 5s. Set to 1s for demonstrative purposes.
+			trace.WithBatchTimeout(time.Second)),
+	)
+	return tracerProvider, nil
+}
+
+func newStdOutMeterProvider(res resource.Resource) (*metric.MeterProvider, error) {
+	metricExporter, err := stdoutmetric.New(
+		stdoutmetric.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(&res),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+			// Default is 1m. Set to 3s for demonstrative purposes.
+			metric.WithInterval(3*time.Second))),
+	)
+	return meterProvider, nil
+}
+
+func newStdOutLoggerProvider(res resource.Resource) (*log.LoggerProvider, error) {
+	logExporter, err := stdoutlog.New(
+		stdoutlog.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	loggerProvider := log.NewLoggerProvider(
+		log.WithResource(&res),
+		log.WithProcessor(log.NewBatchProcessor(logExporter)),
+	)
+	return loggerProvider, nil
+}
+
+func newOtlpTracerProvider(res resource.Resource) (*trace.TracerProvider, error) {
 	traceExporter, err := otlptracehttp.New(context.Background())
 	if err != nil {
 		return nil, err
@@ -186,7 +265,7 @@ func newTracerProvider(res resource.Resource) (*trace.TracerProvider, error) {
 	return tracerProvider, nil
 }
 
-func newMeterProvider(res resource.Resource) (*metric.MeterProvider, error) {
+func newOtlpMeterProvider(res resource.Resource) (*metric.MeterProvider, error) {
 	metricExporter, err := otlpmetrichttp.New(context.Background())
 	if err != nil {
 		return nil, err
@@ -201,7 +280,7 @@ func newMeterProvider(res resource.Resource) (*metric.MeterProvider, error) {
 	return meterProvider, nil
 }
 
-func newLoggerProvider(res resource.Resource) (*log.LoggerProvider, error) {
+func newOtlpLoggerProvider(res resource.Resource) (*log.LoggerProvider, error) {
 	logExporter, err := otlploghttp.New(context.Background())
 	if err != nil {
 		return nil, err
